d7a57374dd03ac3db5c8e07c0fe7099a15f718b7
LyricLy <gulliverhanson@gmail.com>
2020-07-31 00:13:37 +1200

    appease umnikos

--- anime.py
+++ anime.py
@@ -1,12 +1,4 @@
-import string
-
-
-def triples(s):
-    l = [c for c in s.lower() if c in string.ascii_lowercase + string.digits]
-    return {"".join(l[i:i+3]) for i in range(0, len(l)-2)}
-
-def trigram(x, y):
-    return len(triples(x) & triples(y)) / len(triples(x) | triples(y))
+from fuzzywuzzy import fuzz
 
 
 class Anime:
@@ -25,6 +17,6 @@ class Anime:
 
     def match(self, s):
         if self.roma is not None:
-            return max(trigram(self.name, s), trigram(self.roma, s))
+            return max(fuzz.ratio(self.name, s), fuzz.ratio(self.roma, s))
         else:
-            return trigram(self.name, s)
+            return fuzz.ratio(self.name, s)
